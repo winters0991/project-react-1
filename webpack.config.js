ca8c7c514583268218550a382a1390d78fbc2b94
winters0991 <499340367@qq.com>
2017-04-15 21:26:24 +0800

    压缩bundle.js >> bundle-min.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-var path = require('path');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './app/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle-min.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+		new webpack.optimize.UglifyJsPlugin({//引入插件
+			compress: {
+				warnings: false
+			}
+		})
+  ]
 };
